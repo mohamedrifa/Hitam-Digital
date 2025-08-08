3636c65d631c8c0d2573b15e04063e7958275289
rahulprasanna012 <rahulprasanna012@gmail.com>
2025-08-08 16:26:13 +0530

    case study

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import BlogSection from "../components/home/BlogSection";
 import CTASection from "../components/home/CTASection";
 import Footer from "../template/Footer";
+import CaseStudySection from "../components/case_study/CaseStudySection";
 
 // ✅ lazy() must also be a function here
 const OurClients = lazy(() => import("../components/home/OurClients"));
@@ -16,6 +17,7 @@ const HomePage = () => {
         <FAQSection/>
         <BlogSection/>
         <CTASection/>
+        <CaseStudySection/>
         <Footer/>
       </Suspense>
     </div>
