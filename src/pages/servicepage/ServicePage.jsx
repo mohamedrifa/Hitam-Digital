864859881fcdaaf8870fd4a9e5b70a50248e4279
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-10-29 20:55:51 +0530

    bhagddhja

--- src/pages/servicepage/ServicePage.jsx
+++ src/pages/servicepage/ServicePage.jsx
@@ -8,7 +8,6 @@ import BlogSection from "../../components/home/BlogSection";
 import ServiceSection from "../../components/servicesection/ServiceCardde";
 
 const ServicePage = () => {
-   
  return(
     <>
     <HeroSectionOne/>
